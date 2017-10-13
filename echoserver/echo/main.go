8f750d414cae8bb00ac5831832a5c66d8de54c34
njdaniel <nicholasjdaniel@gmail.com>
2017-10-12 23:28:48 -0400

    add handle and echo

--- echoserver/echo/main.go
+++ echoserver/echo/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"net"
 	"log"
+	"bufio"
+	"time"
+	"fmt"
+	"strings"
 )
 
 func main() {
@@ -21,5 +25,17 @@ func main() {
 }
 
 func handleConn(c net.Conn)  {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
 
+func echo(c net.Conn, shout string, delay time.Duration)  {
+	fmt.Println(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
 }
