f738556c4aa5b4306e15069018e7d9681d071791
njdaniel <nicholasjdaniel@gmail.com>
2017-10-15 10:00:06 -0400

    add switch

--- server/main.go
+++ server/main.go
@@ -5,7 +5,8 @@ import (
 	"log"
 	"flag"
 	"fmt"
-	//"bufio"
+	"bufio"
+	"strings"
 )
 
 type conn struct {
@@ -23,7 +24,50 @@ func NewConn(cmdConn net.Conn) *conn {
 
 func (c *conn) run()  {
 	fmt.Fprintln("220 Ready.")
-	//s := bufio.NewScanner(c.rw)
+	s := bufio.NewScanner(c.rw)
+	var cmd string
+	var args []string
+	for s.Scan() {
+		if c.CmdErr() != nil {
+			log.Print("err:", fmt.Errorf("command connection: %s", c.CmdErr()))
+		}
+		fields := strings.Fields(s.Text())
+		if len(fields) == 0 {
+			continue
+		}
+		cmd = strings.ToUpper(fields[0])
+		args = nil
+		if len(fields) > 1 {
+			args = fields[1:]
+		}
+		switch cmd {
+		case "LIST":
+			c.list(args)
+		case "NOOP":
+			fmt.Fprintln("200 Ready.")
+		case "PASV":
+			c.pasv(args)
+		case "PORT":
+			c.port(args)
+		case "QUIT":
+			fmt.Fprintln("221 Goodbye.")
+			return
+		case "RETR":
+			c.retr(args)
+		case "STOR":
+			c.stor(args)
+		case "STRU":
+			c.stru(args)
+		case "SYST":
+			fmt.Fprintln("215 UNIX Type: L8")
+		case "TYPE":
+			c.type_(args)
+		case "USER":
+			fmt.Fprintln("230 Login successful.")
+		default:
+			fmt.Fprintln("502 Command not implemented")
+		}
+	}
 }
 
 func main() {
