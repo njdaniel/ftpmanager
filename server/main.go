667664a2a8ef4fb7e34411497c05f0213749d559
njdaniel <nicholasjdaniel@gmail.com>
2017-10-15 16:44:31 -0400

    add host

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"bufio"
 	"strings"
+	"strconv"
 )
 
 type conn struct {
@@ -20,6 +21,34 @@ type conn struct {
 
 func NewConn(cmdConn net.Conn) *conn {
 	return &conn{rw: cmdConn}
+}
+
+func hostPortToFTP(hostport string) (addr string, err error) {
+	host, portStr, err := net.SplitHostPort(hostport)
+	if err != nil {
+		return "", err
+	}
+	ipAddr, err := net.ResolveIPAddr("ip4", host)
+	if err != nil {
+		return "", err
+	}
+	port, err := strconv.ParseInt(portStr, 10, 64)
+	if err != nil {
+		return "", err
+	}
+	ip := ipAddr.IP.To4()
+	s := fmt.Sprintf("%d,%d,%d,%d,%d,%d", ip[0], ip[1], ip[2], ip[3], port/256, port%256)
+	return s, nil
+}
+
+func hostPortFromFTP(address string) (string, error) {
+	var a, b, c, d byte
+	var p1, p2 int
+	_, err := fmt.Sscanf(address, "%d%,d,%d,%d,%d,%d", &a, &b, &c, &d, &p1, &p2)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%d.%d.%d.%d:%d", a, b, c, d, 256*p1+p2), nil
 }
 
 func (c *conn) run()  {
